75c4ee94fab512ca9ddd0c17c7628aff2fd3b2d9
serendatapy <arvaldivia@gmail.com>
2020-07-20 12:55:38 +0200

    cleaning up real project

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
-import React, { useState } from 'react';
-import Counter from './Counter'
-import CounterHooks from './CounterHooks'
-
-export const ThemeContext = React.createContext();
-//consumer = provider
+import React from 'react';
 
 function App() {
-  const [theme, setTheme] = useState('green')
-  return (
-    <ThemeContext.Provider value={{ backgroundColor: theme }}>
-    Counter
-    <Counter initialCount={0}/>
-    Counter Hooks
-    <CounterHooks initialCount={0}/>
-    <button onClick = {()=> setTheme(prevTheme => {
-      return prevTheme === 'red' ? 'blue' : 'red'})}>Toggle Theme</button>
-    </ThemeContext.Provider>
-  )
+  return null
 }
-
 export default App;
 
 /*
-Themes are a way of setting global variables, something that CAN'T
-be done in react!
 
-This way, all components within a theme, however deeply nested,
-will have access to those variables.
-
-Everything within ThemeContext will have access to values provided
 */
